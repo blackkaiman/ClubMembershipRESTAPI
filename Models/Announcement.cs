9826334e3864a0c3a3c9541393011cf887f02149
Kaiman <demetriustalos@gmail.com>
2021-11-23 16:22:48 +0200

    Add project files.

---
+++ Models/Announcement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectPractica.Models
+{
+    public class Announcement
+    {
+        public Guid IdAnnouncement { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime EventDateTime { get; set; }
+        public string Tags { get; set; }
+    }
+}
---
+++ Models/CodeSnippet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectPractica.Models
+{
+    public class CodeSnippet
+    {
+        public Guid IdCodeSnippet { get; set; }
+        public string Title { get; set; }
+        public string ContentCode { get; set; }
+        public Guid IdMember { get; set; }
+        public int Revision { get; set; }
+        public bool IsPublished { get; set; }
+        public DateTime DateTimeAdded { get; set; }
+    }
+}
