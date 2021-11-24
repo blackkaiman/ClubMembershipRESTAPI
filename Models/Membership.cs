c7259ec80093c0597e55614f3257508ed719286c
Kaiman <demetriustalos@gmail.com>
2021-11-24 11:43:26 +0200

    Added 3 classes

---
+++ Models/Membership.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectPractica.Models
+{
+    public class Membership
+    {
+        public Guid IdMembership { get; set; }
+        public Guid IdMember { get; set; }
+        public Guid IdMembershipType { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Level { get; set; }
+    }
+}
